7287c122bdafc79bd12ff0e2265e12392c3ec435
Samuel Dudley <sbarkerdudley@gmail.com>
2022-02-04 00:01:54 -0800

    React Router basic setup, and aggregate file for components

---
+++ client/src/components/StyleProvider.jsx
@@ -0,0 +1,11 @@
+import theme from '../styles/theme';
+import React from 'react';
+import { MantineProvider } from '@mantine/core';
+
+export default function StyleProvider({ children }) {
+  return (
+    <MantineProvider withGlobalStyles theme={theme}>
+      {children}
+    </MantineProvider>
+  )
+}
---
+++ client/src/components/index.js
@@ -0,0 +1,56 @@
+import Album from './Album';
+import AlbumGrid from './AlbumGrid';
+import AlbumImage from './AlbumImage';
+import AlbumList from './AlbumList';
+import AppHeader from './AppHeader';
+import Buttons from './Buttons';
+import CardBody from './CardBody';
+import ColorProvider from './ColorProvider';
+import ColorSelector from './ColorSelector';
+import Fav from './Fav';
+import Footer from './Footer';
+import Gradient from './Gradient';
+import Item from './Item';
+import ItemText from './ItemText';
+import Login from './Login';
+import ScrollButton from './ScrollButton';
+import SearchProvider from './SearchProvider';
+import Seeds from './Seeds';
+import SpotifyAPIContext from './SpotifyAPIContext';
+import SlidersContainer from './SlidersContainer';
+import SlidersDrawer from './SlidersDrawer';
+import SliderGenerator from './SliderGenerator';
+import SpotifyProvider from './SpotifyProvider';
+import StyleProvider from './StyleProvider';
+import Thumb from './Thumb';
+import UserAvatar from './UserAvatar';
+
+
+export {
+  Album,
+  AlbumGrid,
+  AlbumImage,
+  AlbumList,
+  AppHeader,
+  Buttons,
+  CardBody,
+  ColorProvider,
+  ColorSelector,
+  Fav,
+  Footer,
+  Gradient,
+  Item,
+  ItemText,
+  Login,
+  ScrollButton,
+  SearchProvider,
+  Seeds,
+  SpotifyAPIContext,
+  SlidersContainer,
+  SlidersDrawer,
+  SliderGenerator,
+  SpotifyProvider,
+  StyleProvider,
+  Thumb,
+  UserAvatar
+};
