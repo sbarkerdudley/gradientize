183dd71629377ff7e10835d75bccf14ca8ec6af3
Samuel Dudley <sbarkerdudley@gmail.com>
2022-02-22 09:37:29 -0800

    Removed check for 'window' object that allegedly can cause bugs with netlify

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import Main from './main';
 
-if (document) {
-  render(
-    <Main />,
-    document.getElementById('root')
-  );
-}
+render(<Main />, document.getElementById('root'));
